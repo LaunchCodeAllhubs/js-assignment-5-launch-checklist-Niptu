2bce27940f1c763f11c85255588ef48353ae9705
Nikolai <nikolaiptukhin@gmail.com>
2023-08-03 21:51:40 -0400

    Almost done, need help

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,39 +1,54 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+//require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   /*
+   document.getElementById ("missionTarget").innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}" alt="Planet ${name} Image">
+                `;
 }
 
 function validateInput(testInput) {
-   
-}
+   if (testInput === ""){
+    return "Empty";
+    }else if (isNaN(Number(testInput))){
+    return "Not a Number";
+    }else {
+    return "Is a Number";
+    }
+};
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        alert ("All fields are required!");
+    }
 }
 
 async function myFetch() {
+ 
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+       response.json().then ( function(json){
+        let planetList = json.planets;
+        console.log (json);
+       });
+    });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let randomIndex = Math.random();
+    return planets[randomIndex];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
